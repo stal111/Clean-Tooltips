2b5745054e9d71698f558996d3414a154a3d5c03
stal111 <stal111@gmx.de>
2023-08-13 22:19:48 +0200

    Update loom version

--- fabric/src/main/java/net/mandalacreations/clean_tooltips/fabric/CleanTooltipsFabric.java
+++ fabric/src/main/java/net/mandalacreations/clean_tooltips/fabric/CleanTooltipsFabric.java
@@ -1,14 +1,15 @@
 package net.mandalacreations.clean_tooltips.fabric;
 
 import fuzs.forgeconfigapiport.api.config.v2.ForgeConfigRegistry;
+import net.fabricmc.api.ClientModInitializer;
 import net.mandalacreations.clean_tooltips.CleanTooltips;
 import net.fabricmc.api.ModInitializer;
 import net.mandalacreations.clean_tooltips.client.config.ClientConfig;
 import net.minecraftforge.fml.config.ModConfig;
 
-public class CleanTooltipsFabric implements ModInitializer {
+public class CleanTooltipsFabric implements ClientModInitializer {
     @Override
-    public void onInitialize() {
+    public void onInitializeClient() {
         CleanTooltips.init();
 
         ForgeConfigRegistry.INSTANCE.register(CleanTooltips.MOD_ID, ModConfig.Type.CLIENT, ClientConfig.SPEC);
